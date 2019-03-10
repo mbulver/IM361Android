d12ed1b06408b47230fd066e6b1924e18c54ad79
mbulver <46090670+mbulver@users.noreply.github.com>
2019-03-10 13:41:08 -0500

    Assignment
    Fixed errors and added interactivity

--- app/src/main/java/com/maxbulver/planmo/MainActivity.kt
+++ app/src/main/java/com/maxbulver/planmo/MainActivity.kt
@@ -2,11 +2,18 @@ package com.maxbulver.planmo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var myText: TextView = findViewById(R.id.my_text_view)
+        var myButton: Button = findViewById(R.id.my_button)
+        myButton.setOnClickListener {
+            myText.text = "I've been clicked!"
+        }
     }
 }
