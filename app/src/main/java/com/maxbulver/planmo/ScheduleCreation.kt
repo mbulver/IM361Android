514922118dcca7ab5ce98a2b3c8a25b43add7a85
mbulver <46090670+mbulver@users.noreply.github.com>
2019-04-02 16:49:45 -0500

    Added a spinner
    WIll continue to add more things throughout the day

--- app/src/main/java/com/maxbulver/planmo/ScheduleCreation.kt
+++ app/src/main/java/com/maxbulver/planmo/ScheduleCreation.kt
@@ -2,10 +2,23 @@ package com.maxbulver.planmo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+enum class EventType {
+    Academic,
+    Social,
+    Volunteer
+
+}
 
 class ScheduleCreation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.schedule_creation)
+        val spinner: Spinner = findViewById(R.id.spinner)
+        spinner.adapter = ArrayAdapter<EventType>(this,
+            android.R.layout.simple_spinner_item, EventType.values())
+        val type = spinner.selectedItem as EventType
     }
 }
